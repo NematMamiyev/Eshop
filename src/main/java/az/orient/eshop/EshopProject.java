f819fc3b19b34d8392ce3cc0c96d827de5951cd4
nemat-mamiyev <nemet.memiyev1@gmail.com>
2025-01-11 21:05:11 +0400

    Rename

---
+++ src/main/java/az/orient/eshop/EshopProject.java
@@ -0,0 +1,12 @@
+package az.orient.eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopProject {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EshopProject.class, args);
+    }
+}
--- src/test/java/az/orient/eshop/EshopProjectTests.java
+++ src/test/java/az/orient/eshop/EshopProjectTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EshopSpring2ApplicationTests {
+class EshopProjectTests {
 
     @Test
     void contextLoads() {
